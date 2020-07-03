f9ea146c11c5570065717b1ea33bd4e97d7bc047
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-03 12:08:21 -0300

    feat(routing): Created user creation route.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from models.user import User
+from random import randint
 
 app = FastAPI()
 
 
-@app.get('/')
-async def root():
-    return {"message": "Hello World"}
+@app.post('/users')
+async def create_item(user: User):
+    user_dict = user.dict()
+    user_dict.update({"user_id": randint(1, 100000)})
+    return user_dict
