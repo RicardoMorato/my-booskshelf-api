089413da5e0b244d131bf7039eb1c091a455a2c9
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-04 21:20:09 -0300

    feat(User-schema): Added example schema.

--- src/models/user.py
+++ src/models/user.py
@@ -3,3 +3,10 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     name: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Luke Skywalker"
+            }
+        }
