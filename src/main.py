e81b800280446d5d614137cf3a85947e6f0b80f7
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-04 21:20:23 -0300

    feat(root): Added root path.

--- src/main.py
+++ src/main.py
@@ -5,6 +5,16 @@ from random import randint
 app = FastAPI()
 
 
+@app.get('/')
+async def root():
+    info = {
+        'App': 'Undefined',
+        'Status': 'Development',
+        'Author': 'Ricardo Morato <https://github.com/RicardoMorato>'
+    }
+    return info
+
+
 @app.post('/users')
 async def create_item(user: User):
     user_dict = user.dict()
