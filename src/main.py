51d9c43030fdeef01ca529a7543da9903fc7a6b3
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-05 16:34:56 -0300

    feat(search-books): Added route to search books.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+import requests
 from models.user import UserIn, UserOut
+from config import Settings
+
+settings = Settings()
+print(settings.api_key)
 
 app = FastAPI()
 
@@ -14,6 +19,14 @@ async def root():
     return info
 
 
-@app.post("/user/", response_model=UserOut)
+@app.post('/user', response_model=UserOut)
 async def create_user(user: UserIn):
     return user
+
+
+@app.get('/search/{search_param}')
+async def search_google(search_param):
+    url = f'https://www.googleapis.com/books/v1/volumes?q={search_param}'
+    res = requests.get(url)
+    volume_info = res.json()["items"][0]["volumeInfo"]
+    return volume_info
